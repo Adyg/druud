87c5f6fa70c0e2ac038247fef430968abd13198d
Adyg <ady.ghiuta@gmail.com>
2016-01-23 00:43:12 +0200

    get rid of dir change

--- publish_assets.sh
+++ publish_assets.sh
@@ -1,4 +1,3 @@
-cd druud && \
 env SECRET_KEY=`heroku config:get SECRET_KEY --app=druud` \
     AWS_SECRET_ACCESS_KEY=`heroku config:get AWS_SECRET_ACCESS_KEY --app=druud` \
     AWS_ACCESS_KEY_ID=`heroku config:get AWS_ACCESS_KEY_ID --app=druud` \
